5dbc6996d997930ae9ca6e3aa408b5ac640fe9a7
CuongVnZ <edgcuong@gmail.com>
2023-08-19 05:16:31 +0700

    Update schema user

--- models/User.js
+++ models/User.js
@@ -4,10 +4,11 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    name: {type: String, required: true},
+    fullname: {type: String, required: true},
     phone: {type: String, required: true, unique: true},
     shippingAddress: {type: String, required: true},
     password: { type: String, required: true },
+    points: {type: Number, default: 0},
     isAdmin: {
       type: Boolean,
       default: false,
