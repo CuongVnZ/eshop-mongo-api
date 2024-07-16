83ac1461ae36d539b0c5b944f4c6eb2debc8ada0
CuongVnZ <edgcuong@gmail.com>
2024-07-16 21:32:38 +1000

    Update stripe

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -1,24 +1,34 @@
 import { Router, Request, Response } from "express";
-// import Stripe from "stripe";
+import Stripe from "stripe";
+const stripe = new Stripe("sk_test_51LOeVQEuYDk3LNJoUMW12VAK1QrKdDXqI6TsXxWBJCtxSrzNXGubevjffVDCk4aSwFEzQQnKb4fwPIxaAQOazXTc00xh2AipPi");
 
 const router: Router = Router();
-// const stripe = new Stripe("sk_test_51LOeVQEuYDk3LNJoUMW12VAK1QrKdDXqI6TsXxWBJCtxSrzNXGubevjffVDCk4aSwFEzQQnKb4fwPIxaAQOazXTc00xh2AipPi");
-
-// router.post("/payment", (req: Request, res: Response) => {
-//   stripe.charges.create(
-//     {
-//       source: req.body.tokenId,
-//       amount: Math.floor(req.body.amount),
-//       currency: "usd",
-//     },
-//     (stripeErr: Stripe.StripeError | null, stripeRes: Stripe.Response<Stripe.Charges.Charge>) => {
-//       if (stripeErr) {
-//         console.log(stripeErr);
-//         return res.status(500).json(stripeErr);
-//       }
-//       return res.status(200).json(stripeRes);
-//     }
-//   );
-// });
+router.post("/payment", (req: Request, res: Response) => {
+  stripe.customers.create({
+    email: req.body.email,
+    source: req.body.tokenId
+  })
+  .then(customer => {
+    return stripe.invoiceItems.create({
+      customer: customer.id,
+      amount: Math.floor(req.body.amount),
+      currency: "usd",
+      description: "Test Payment"
+    });
+  })
+  .then(invoiceItem => {
+    return stripe.invoices.create({
+      collection_method: "send_invoice",
+      customer: invoiceItem.customer as string
+    });
+  })
+  .then(invoice => {
+    
+  })
+  .catch(err => {
+    console.log(err);
+    return res.status(500).json(err);
+  });
+});
 
 export default router;
